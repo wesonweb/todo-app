bb5b6ada6aa90b3e1992cee64af7775d0b4ea2dd
Wes Mehaffy <wesonweb@gmail.com>
2023-06-13 14:07:11 +0100

    adds basic form to add a todo and adds react icons package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
 function App() {
+  const handleSubmit = (event) => {}
   return (
     <>
       <h1>Todo app</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add a new todo"
+        />
+        <button
+          type="submit">
+          Add
+        </button>
+      </form>
     </>
   );
 }
